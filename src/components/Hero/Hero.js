1048bdf0612c72ba8e465071f52d893524e5fc70
mento-mume <akpankevman@gmail.com>
2023-05-17 00:20:33 +0100

    finalized portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle>
+        Hi i'm
+        <br />
+        Ndukeabasi Etim
+      </SectionTitle>
+      <SectionText>
+        I am a passionate full-stack web developer with two years of experience.
+        I specialize in utilizing React, Next.js, and Node.js to build dynamic
+        and interactive web applications
+      </SectionText>
+      <Button>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
