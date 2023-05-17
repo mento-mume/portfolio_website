7cd3927f4ad4f1161cc8f91a2fb2b02a6e5efb30
mento-mume <akpankevman@gmail.com>
2023-05-17 14:32:59 +0100

    legacy props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ import {
 const Header = () => (
   <Container>
     <Div1>
-      <Link href="#">
+      <Link legacyBehavior href="#">
         <a
           style={{
             display: "flex",
@@ -35,17 +35,17 @@ const Header = () => (
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
+        <Link legacyBehavior href="#projects">
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#tech">
+        <Link legacyBehavior href="#tech">
           <NavLink>Technologies</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#about">
+        <Link legacyBehavior href="#about">
           <NavLink>About</NavLink>
         </Link>
       </li>
